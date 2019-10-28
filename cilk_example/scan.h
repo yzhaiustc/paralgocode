1e27ff61fc82d2dcc26d7bc27cb05ccbd5cf376f
Yihan Sun <yihans@cs.cmu.edu>
2019-10-28 19:43:45 -0400

    changes

--- cilk_example/scan.h
+++ cilk_example/scan.h
@@ -4,11 +4,26 @@ void scan(int* In, int* Out, int* B, int* C, int n) {
 		Out[0] = In[0];
 		return;
 	}
+	cout << "In:" << endl;
+	for (int i = 0; i < n; i++) cout << In[i] << " ";
+	cout << endl;
+	
 	cilk_for (int i = 0; i < n/2; i++) B[i] = In[2*i] + In[2*i+1];
+	cout << "B:" << endl;
+	for (int i = 0; i < n/2; i++) cout << B[i] << " ";
+	cout << endl;
+	
 	scan(B, C, B+n/2, C+n/2, n/2);
+	cout << "C:" << endl;
+	for (int i = 0; i < n/2; i++) cout << In[i] << " ";
+	cout << endl;
+	
 	Out[0] = In[0];
 	cilk_for (int i = 1; i < n; i++) {
 		if (i%2) Out[i] = C[(i-1)/2] + In[i];
 		else Out[i] = C[i/2];
 	}
+	cout << "Out:" << endl;
+	for (int i = 0; i < n; i++) cout << Out[i] << " ";
+	cout << endl;
 }
