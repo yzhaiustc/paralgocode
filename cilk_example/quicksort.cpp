aa24d471c30fea8c8044ca123cc6b0e286384788
Yihan Sun <yihans@cs.cmu.edu>
2019-10-28 21:31:04 -0400

    changes

--- cilk_example/quicksort.cpp
+++ cilk_example/quicksort.cpp
@@ -23,11 +23,11 @@ int main(int argc, char** argv) {
 	int* A = new int[n];
 	cilk_for (int i = 0; i < n; i++) A[i] = (hash32(i)) % (n*2);
 	
-	A2 = new int[end-start];
-	B = new int[end-start];
-	F = new int[end-start];
-	e1 = new int[end-start];
-	e2 = new int[end-start];
+	A2 = new int[n];
+	B = new int[n];
+	F = new int[n];
+	e1 = new int[n];
+	e2 = new int[n];
 	
 	timer t; t.start();
 	qsort(A, 0, n);
