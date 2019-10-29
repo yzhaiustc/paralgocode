a6fc32caa35788d6a5e0cc7fdd24c90d9181bb3a
Yihan Sun <yihans@cs.cmu.edu>
2019-10-28 20:12:27 -0400

    changes

--- cilk_example/scan.h
+++ cilk_example/scan.h
@@ -1,38 +1,18 @@
 using namespace std;
 
 void scan(int* In, int* Out, int* B, int* C, int n) {
-	cout << "n = " << n << endl;
 	if (n==0) return;
 	if (n==1) {
 		Out[0] = In[0];
 		return;
 	}
-	cout << "In:" << endl;
-	for (int i = 0; i < n; i++) cout << In[i] << " ";
-	cout << endl;
-	
 	cilk_for (int i = 0; i < n/2; i++) B[i] = In[2*i] + In[2*i+1];
-	cout << "B:" << endl;
-	for (int i = 0; i < n/2; i++) cout << B[i] << " ";
-	cout << endl;
 	
-	scan(B, C, B+n/2, C+n/2, n/2);
-	cout << "C:" << endl;
-	for (int i = 0; i < n/2; i++) cout << C[i] << " ";
-	cout << endl;
-	
+	scan(B, C, B+n/2, C+n/2, n/2);	
 	Out[0] = In[0];
-	//Out[1] = In[0] + In[1];
-	cout << "In:" << endl;
-	for (int i = 0; i < n; i++) cout << In[i] << " ";
-	cout << endl;
 	
 	cilk_for (int i = 1; i < n; i++) {
-		//cout << i << " " << C[(i-1)/2] << " " << In[i] << endl;
 		if (i%2) Out[i] = C[i/2];
 		else Out[i] = C[i/2-1] + In[i];
 	}
-	cout << "Out:" << endl;
-	for (int i = 0; i < n; i++) cout << Out[i] << " ";
-	cout << endl;
 }
