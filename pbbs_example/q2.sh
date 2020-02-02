b211cbe8e379d37c592db06e4c23f1a0f2c4af6a
yzhaiustc <yzhai015@ucr.edu>
2020-02-02 00:25:40 -0500

    q5-1

--- pbbs_example/q2.sh
+++ pbbs_example/q2.sh
@@ -4,25 +4,26 @@
 make clean && make
 rm *.txt
 
-len=5000000
 
 declare -a algo=("reduce" "scan")
 
 num_of_algo=${#algo[@]}
 
-for (( i=0; i<${num_of_algo}; i++ ));
+for len in {5000000..6000000..50000}
 do
-    res_file=res_${algo[$i]}.txt
-    echo $res_file
-    ./seq_${algo[$i]} $len &> $res_file
-    for thread in {1..64..1}
+    for (( i=0; i<${num_of_algo}; i++ ));
     do
-        for n in {1..10..1}
+        res_file=res_${algo[$i]}.txt
+        echo $res_file
+        ./seq_${algo[$i]} $len &> $res_file
+        for thread in {1..64..1}
+        echo $thread
         do
-            export NUM_THREADS=${thread}
-            echo $NUM_THREADS
-            ./${algo[$i]} $len >> $res_file
+            for n in {1..20..1}
+            do
+                export NUM_THREADS=${thread}
+                ./${algo[$i]} $len >> $res_file
+            done
         done
     done
-  
 done
